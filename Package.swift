919ce1bd104450d34e0310804c50c33f34c27075
David Stephens <git@dwrs.org>
2025-12-09 00:54:09 +0000

    Depends on Snailed-It version of Swinject package

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "SwinjectStoryboard", targets: ["SwinjectStoryboard"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swinject/Swinject.git", .upToNextMajor(from: "2.7.1")),
+        .package(url: "https://github.com/Snailed-It/Swinject.git", .branch("restore-loggingFunction")),
     ],
     targets: [
         .target(
